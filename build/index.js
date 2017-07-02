9e428aaa797d917e2e2464b43e9e083cf6fe6c62
Wanderrful <galois@mentlegen.com>
2017-07-02 22:44:28 +0900

    Removed the "build" folder, which contained only the compiled JavaScript and is therefore redundant

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const arma_bot_1 = require("./arma_bot");
-let Bot = new arma_bot_1.default();
-Bot.login("Mjk2OTMzMTUzMDE5Mzk2MDk3.C75hLg.CtRd87FwV8tZZ8Jytu5T5VtpPAg");
-//# sourceMappingURL=index.js.map
