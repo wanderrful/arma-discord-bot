71b8cb01fe5e243f1ef8708a1cb6679c184441b8
Wanderrful <galois@mentlegen.com>
2017-07-07 01:45:11 +0900

    Continuing to implememt Heroku compatibility

--- build/index.js
+++ build/index.js
@@ -2,6 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const arma_bot_1 = require("./arma_bot");
 const bot_key_1 = require("./bot_key");
-let Bot = new arma_bot_1.default();
-Bot.login(bot_key_1.default);
+function default_1() {
+    let Bot = new arma_bot_1.default();
+    Bot.login(bot_key_1.default);
+}
+exports.default = default_1;
 //# sourceMappingURL=index.js.map
