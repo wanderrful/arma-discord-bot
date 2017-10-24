561749109b5d161d17cfcee79b9d38c79ee44a17
Wanderrful <galois@mentlegen.com>
2017-10-25 08:31:47 +0900

    Removed deprecated files

--- build/bot_key.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let BotKey = "Mjk2OTMzMTUzMDE5Mzk2MDk3.DDqQvw.Q1lKEFvZUAh2zpA2BtMiaIdMDYI";
-exports.default = BotKey;
-//# sourceMappingURL=bot_key.js.map
